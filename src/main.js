83b563e404104c24404cf76270e4fed63ccfddff
Ervin <70893070+Er4ik@users.noreply.github.com>
2022-10-27 20:30:30 +0300

    fix: fixed

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import http from 'http';
-const PORT = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Er4ik');
-});
-server.listen(`0.0.0.0:${PORT}`, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
-});
